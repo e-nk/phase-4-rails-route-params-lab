c8a5617645b4443276be2e13c5314f0804edb4b3
e-nk <enocklagat747@gmail.com>
2023-03-18 12:53:46 +0300

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,19 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def grades
+    grades = Student.all.order(grade: :desc)
+    render json: grades
+  end
+
+  def highest_grade
+    highest_grade = Student.all.order(grade: :desc).first
+    render json: highest_grade
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 
 end
